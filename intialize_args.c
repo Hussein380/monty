e837710a31f701bcdc6634ec05d9cc797052b00a
Hussein380 <huznigarane@gmail.com>
2024-01-17 04:39:05 -0800

    created tokenize.c

--- intialize_args.c
+++ intialize_args.c
@@ -12,6 +12,16 @@ void initialize_arguments(void)
 		free_arguments();
 		exit(EXIT_FAILURE);
 	}
+	arguments->instruction = malloc(sizeof(instruction_t));
+	if (arguments->instruction == NULL)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_arguments();
+		exit(EXIT_FAILURE);
+	}
 	arguments->stream = NULL;
 	arguments->line = NULL;
+	arguments->n_tokens = 0;
+	arguments->line_number = 0;
+
 }
